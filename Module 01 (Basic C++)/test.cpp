895bc6d43582649f7feb4db0a0a7ddaaedf1fcb9
emonShimoul <emonshimoul@gmail.com>
2025-07-30 14:22:58 +0600

    first commit

---
+++ Module 01 (Basic C++)/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+    int x = 200;
+    char c = 'A';
+    double d = 34.56;
+    std::cout << x << " " << c << std::endl << d << "\n";
+    std::cout << "My fav number is " << x << std::endl;
+    return 0;
+}
