c07785dbff414b2629b7b2c1befbc17599364a3f
emonShimoul <emonshimoul@gmail.com>
2025-08-07 01:22:09 +0600

    this keyword in c++

---
+++ Module 03 (Class and Object)/thisAndArrowSign.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa){
+        this->roll = roll;
+        (*this).cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+int main()
+{
+    Student rahim(4,5,4.2);
+    Student karim(2,5,4.8);
+
+    cout << rahim.roll << " " << rahim.cls << " " << rahim.gpa << endl;
+    cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+    
+    return 0;
+}
